7732a5ba7b06a3b7c8e3ebff68623965278d152e
unknown <julian55558@hotmail.com>
2023-03-12 02:45:35 -0500

    e2e test for Store created

--- src/test/java/io/swagger/petstore/store/StoreTests.java
+++ src/test/java/io/swagger/petstore/store/StoreTests.java
@@ -1,8 +1,11 @@
 package io.swagger.petstore.store;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.swagger.petstore.utils.JsonMapper;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,23 +28,28 @@ public class StoreTests {
 
 
     @Test
-    @DisplayName("Should test")
-    public void addNewPet() {
-        StoreGetRequests.inventoriesByStatus().then().statusCode(200).log().all();
+    @DisplayName("Should list,place order,delete and verify the store data")
+    public void e2eTest() {
+        int initialValue =
+                StoreGetRequests.inventoriesByStatus().then().statusCode(200).log().all()
+                        .extract().response().jsonPath().getInt(placeOrderModel.getString("status"));
+
 
         StorePostRequests.placePetOrder(placeOrderBody);
 
-        StoreGetRequests.inventoriesByStatus().then().statusCode(200).log().all();
-
-        StoreDeleteRequests.orderById(1);
-
-        StoreGetRequests.inventoriesByStatus().then().statusCode(200).log().all();
+        int finalValue =
+                StoreGetRequests.inventoriesByStatus().then().statusCode(200).log().all()
+                        .extract().jsonPath().getInt(placeOrderModel.getString("status"));
 
 
-    }
+        Assertions.assertNotEquals(initialValue, finalValue);
 
-    @Test
-    public void e2eTest() {
+        StoreDeleteRequests.orderById(placeOrderModel.getInt("id"));
+
+        Response response = StoreGetRequests.inventoriesByStatus().then()
+                .statusCode(200).log().all().extract().response();
+
+        Assertions.assertEquals(response.jsonPath().getInt(placeOrderModel.getString("status")), initialValue);
 
 
     }
